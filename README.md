97708565e67bb69a683877539cfa110fea9b9485
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-06-21 01:31:02 +0200

    first commit
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Anti Money Laundering
+
+**How to create and start a new virtual environment**
+
+1. `source create_venv.sh venv` 
+
+**How to deactivate the current virtual environment**
+
+1. `deactivate`
---
+++ create_venv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+venv_dir="$1"
+python3 -m venv "$venv_dir"
+echo "Virtual environment created at $venv_dir"
+
+
+activate_script=""
+if [[ $OSTYPE == "msys" || $OSTYPE == "cygwin" || $OSTYPE == "win32" ]]; then
+    activate_script="$venv_dir/Scripts/activate"
+else
+    activate_script="$venv_dir/bin/activate"
+fi
+
+source "$1/bin/activate"
+
+echo "Installing packages..."
+pip install jedi
+pip install numpy 
+pip install pandas 
+pip install pyspark
+echo "Packages installed successfully!"
+pip -V
