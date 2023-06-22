7ea2fd8154e0a23c571705f378a01349f8a4f2c6
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2023-06-22 19:55:03 +0200

    Update test.ipynb :smile:
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- mytest.py
+++ mytest.py
@@ -1,3 +1,13 @@
+import matplotlib.pyplot as plt
+
 def print_hello():
     print('Hello')
+
+def print_goodbye():
+    print('Goodbye')
+
+def plot_first_graph():
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
 
